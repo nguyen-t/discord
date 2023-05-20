5a0ccd1bd6fe1cce8f7b76c0c45d3205f00e0cbe
Tuan Nguyen <5609219+nguyen-t@users.noreply.github.com>
2023-05-19 23:58:12 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize';
+
+const {
+  MYSQL_HOST,
+  MYSQL_DATABASE,
+  MYSQL_USER,
+  MYSQL_PASSWORD
+} = process.env;
+const sequelize = new Sequelize({
+  'host': MYSQL_HOST,
+  'database': MYSQL_DATABASE,
+  'username': MYSQL_USER,
+  'password': MYSQL_PASSWORD,
+  'dialect': 'mariadb',
+});
+
+sequelize.authenticate().then(() => {
+  console.log('Sequelize success');
+}).catch(() => {
+  console.log('Sequelized failed');
+});
